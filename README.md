9c2853a72407731a2ff0b90d1a7c715dde20d978
mapledzy <maoozu@outlook.com>
2017-05-15 22:01:37 +0800

    updata

---
+++ README.md
@@ -0,0 +1,7 @@
+# 薄暮幽祈 永夜长歌 — 裁决女神PVE百科全书
+#### 网页的内容版权属于原作者，禁止以任何复制转载，复制。
+由COLG@逝楓_Soultaker负责编写，我则是练习一下网页排版帮忙编辑成网页版
+
+用到的框架有 materialize-css jquery angular ui-router
+
+2017-5-9
---
+++ js/main.js
@@ -0,0 +1,503 @@
+var app = angular.module('app', ['ui.router']);
+app.config(['$stateProvider', '$urlRouterProvider', '$locationProvider', function ($stateProvider, $urlRouterProvider, $locationProvider) {
+	$locationProvider.hashPrefix('Nemesis');
+	$stateProvider
+			.state('home', {
+				url: '/',
+				views: {
+					'container@': {
+						templateUrl: 'html/home.html'
+					},
+					'title@': {
+						template: '序言'
+					}
+				}
+			})
+			.state('guide', {
+				url: '/guide',
+				views: {
+					'container@': {
+						templateUrl: 'html/guide/guide-sub-0.html'
+					},
+					'title@': {
+						template: '职业简介'
+					}
+				}
+			})
+			.state('badge', {
+				url: '/badge',
+				views: {
+					'container@': {
+						templateUrl: 'html/guide/guide-sub-1.html'
+					},
+					'title@': {
+						template: '时装、附魔、徽章'
+					}
+				}
+			})
+			.state('title', {
+				url: '/title',
+				views: {
+					'container@': {
+						templateUrl: 'html/guide/guide-sub-2.html'
+					},
+					'title@': {
+						template: '称号'
+					}
+				}
+			})
+			.state('pet', {
+				url: '/pet',
+				views: {
+					'container@': {
+						templateUrl: 'html/guide/guide-sub-3.html'
+					},
+					'title@': {
+						template: '宠物'
+					}
+				}
+			});
+	$stateProvider
+			.state('novice', {
+				url: '/novice'
+			})
+			.state('novice.0', {
+				url: '/0',
+				views: {
+					'container@': {
+						templateUrl: 'html/items/novice/novice-sub-0.html'
+					},
+					'title@': {
+						template: '入门篇&nbsp;-&nbsp;<small>加点模板</small>'
+					}
+				}
+			})
+			.state('novice.1', {
+				url: '/1',
+				views: {
+					'container@': {
+						templateUrl: 'html/items/novice/novice-sub-1.html'
+					},
+					'title@': {
+						template: '入门篇&nbsp;-&nbsp;<small>装备推荐</small>'
+					}
+				}
+			})
+			.state('novice.2', {
+				url: '/2',
+				views: {
+					'container@': {
+						templateUrl: 'html/items/novice/novice-sub-2.html'
+					},
+					'title@': {
+						template: '入门篇&nbsp;-&nbsp;<small>Buff堆砌</small>'
+					}
+				}
+			});
+	$stateProvider
+			.state('skills', {
+				url: '/skills'
+			})
+			.state('skills.0', {
+				url: '/0',
+				views: {
+					'container@': {
+						templateUrl: 'html/skills/skill_sub_0.html'
+					},
+					'title@': {
+						template: '第三章&nbsp;-&nbsp;<small>技能篇</small>'
+					}
+				}
+			})
+			.state('skills.1', {
+				url: '/1',
+				views: {
+					'container@': {
+						templateUrl: 'html/skills/skill_sub_1.html'
+					},
+					'title@': {
+						template: '第三章&nbsp;-&nbsp;<small>技能篇</small>'
+					}
+				}
+			})
+			.state('skills.2', {
+				url: '/2',
+				views: {
+					'container@': {
+						templateUrl: 'html/skills/skill_sub_2.html'
+					},
+					'title@': {
+						template: '第三章&nbsp;-&nbsp;<small>技能篇</small>'
+					}
+				}
+			})
+			.state('skills.3', {
+				url: '/3',
+				views: {
+					'container@': {
+						templateUrl: 'html/skills/skill_sub_3.html',
+						controller: 'main'
+					},
+					'title@': {
+						template: '第三章&nbsp;-&nbsp;<small>技能篇</small>'
+					}
+				}
+			})
+			.state('skills.ex', {
+				url: '/ex',
+				views: {
+					'container@': {
+						templateUrl: 'html/skills/skill_sub_ex.html'
+					},
+					'title@': {
+						template: '第三章&nbsp;-&nbsp;<small>技能篇</small>'
+					}
+				}
+			});
+	//定义道具分类
+	$stateProvider
+			.state('items', {
+				url: '/items'
+			});
+	//道具 - 武器
+	$stateProvider
+			.state('items.weapon', {
+				url: '/weapon',
+				views: {
+					'container@': {
+						templateUrl: 'html/items/weapon/weapon_index.html'
+					},
+					'title@': {
+						template: '装备篇&nbsp;-&nbsp;<small>武器</small>'
+					}
+				}
+			})
+			.state('items.weapon.criterion', {
+				url: '/criterion',
+				views: {
+					'items': {
+						templateUrl: 'html/criterion.html'
+					},
+					'title@': {
+						template: '装备篇&nbsp;-&nbsp;<small>criterion</small>'
+					}
+				}
+			})
+			.state('items.weapon.dirk', {
+				url: '/dirk',
+				views: {
+					'items': {
+						templateUrl: 'html/items/weapon/types/dirk.html'
+					},
+					'title@': {
+						template: '装备篇&nbsp;-&nbsp;<small>短剑</small>'
+					}
+				}
+			})
+			.state('items.weapon.katana', {
+				url: '/katana',
+				views: {
+					'items': {
+						templateUrl: 'html/items/weapon/types/katana.html'
+					},
+					'title@': {
+						template: '装备篇&nbsp;-&nbsp;<small>太刀</small>'
+					}
+				}
+			})
+			.state('items.weapon.lswd', {
+				url: '/lswd',
+				views: {
+					'items': {
+						templateUrl: 'html/items/weapon/types/lswd.html'
+					},
+					'title@': {
+						template: '装备篇&nbsp;-&nbsp;<small>巨剑</small>'
+					}
+				}
+			})
+			.state('items.weapon.blunt', {
+				url: '/blunt',
+				views: {
+					'items': {
+						templateUrl: 'html/items/weapon/types/blunt.html'
+					},
+					'title@': {
+						template: '装备篇&nbsp;-&nbsp;<small>钝器</small>'
+					}
+				}
+			});
+	//道具 - 防具
+	$stateProvider
+			.state('items.armors', {
+				url: '/armors',
+				views: {
+					'container@': {
+						templateUrl: 'html/items/armors/armors_index.html'
+					},
+					'title@': {
+						template: '装备篇&nbsp;-&nbsp;<small>防具</small>'
+					}
+				}
+			})
+			.state('items.armors.criterion', {
+				url: '/criterion',
+				views: {
+					'items': {
+						templateUrl: 'html/criterion.html'
+					},
+					'title@': {
+						template: '装备篇&nbsp;-&nbsp;<small>criterion</small>'
+					}
+				}
+			})
+			.state('items.armors.lv80', {
+				url: '/lv80',
+				views: {
+					'items': {
+						templateUrl: 'html/items/armors/lv_80.html'
+					},
+					'title@': {
+						template: '装备篇&nbsp;-&nbsp;<small>80级套装</small>'
+					}
+				}
+			})
+			.state('items.armors.lv85', {
+				url: '/lv85',
+				views: {
+					'items': {
+						templateUrl: 'html/items/armors/lv_85.html'
+					},
+					'title@': {
+						template: '装备篇&nbsp;-&nbsp;<small>85级套装</small>'
+					}
+				}
+			})
+			.state('items.armors.lv90a', {
+				url: '/lv90a',
+				views: {
+					'items': {
+						templateUrl: 'html/items/armors/lv_90a.html'
+					},
+					'title@': {
+						template: '装备篇&nbsp;-&nbsp;<small>90级套装（A类）</small>'
+					}
+				}
+			})
+			.state('items.armors.lv90b', {
+				url: '/lv90b',
+				views: {
+					'items': {
+						templateUrl: 'html/items/armors/lv_90b.html'
+					},
+					'title@': {
+						template: '装备篇&nbsp;-&nbsp;<small>90级套装（B类）</small>'
+					}
+				}
+			})
+			.state('items.armors.scattered', {
+				url: '/scattered',
+				views: {
+					'items': {
+						templateUrl: 'html/items/armors/scattered.html'
+					},
+					'title@': {
+						template: '装备篇&nbsp;-&nbsp;<small>散件</small>'
+					}
+				}
+			});
+	//道具 - 首饰
+	$stateProvider
+			.state('items.ring', {
+				url: '/ring',
+				views: {
+					'container@': {
+						templateUrl: 'html/items/ring/ring_index.html'
+					},
+					'title@': {
+						template: '装备篇&nbsp;-&nbsp;<small>首饰</small>'
+					}
+				}
+			})
+			.state('items.ring.criterion', {
+				url: '/criterion',
+				views: {
+					'items': {
+						templateUrl: 'html/criterion.html'
+					},
+					'title@': {
+						template: '装备篇&nbsp;-&nbsp;<small>criterion</small>'
+					}
+				}
+			})
+			.state('items.ring.suit', {
+				url: '/suit',
+				views: {
+					'items': {
+						templateUrl: 'html/items/ring/suit.html'
+					},
+					'title@': {
+						template: '装备篇&nbsp;-&nbsp;<small>首饰</small>'
+					}
+				}
+			})
+			.state('items.ring.scattered', {
+				url: '/scattered',
+				views: {
+					'items': {
+						templateUrl: 'html/items/ring/scattered.html'
+					},
+					'title@': {
+						template: '装备篇&nbsp;-&nbsp;<small>首饰</small>'
+					}
+				}
+			})
+			.state('items.auxiliary', {
+				url: '/auxiliary',
+				views: {
+					'container@': {
+						templateUrl: 'html/items/auxiliary/auxiliary_index.html'
+					},
+					'title@': {
+						template: '装备篇&nbsp;-&nbsp;<small>特殊装备</small>'
+					}
+				}
+			})
+			.state('items.auxiliary.suit', {
+				url: '/suit',
+				views: {
+					'items': {
+						templateUrl: 'html/items/auxiliary/suit.html'
+					},
+					'title@': {
+						template: '装备篇&nbsp;-&nbsp;<small>特殊装备</small>'
+					}
+				}
+			})
+			.state('items.auxiliary.scattered', {
+				url: '/scattered',
+				views: {
+					'items': {
+						templateUrl: 'html/items/auxiliary/scattered.html'
+					},
+					'title@': {
+						template: '装备篇&nbsp;-&nbsp;<small>特殊装备</small>'
+					}
+				}
+			})
+			.state('items.degenerator', {
+				url: '/degenerator',
+				views: {
+					'container@': {
+						templateUrl: 'html/degenerator/degenerator-index.html'
+					},
+					'title@': {
+						template: '装备篇&nbsp;-&nbsp;<small>武器</small>'
+					}
+				}
+			})
+			.state('items.degenerator.2&3', {
+				url: '/2&3',
+				views: {
+					'items': {
+						templateUrl: 'html/degenerator/degenerator-sub-0.html'
+					},
+					'title@': {
+						template: '装备篇&nbsp;-&nbsp;<small>异界装备</small>'
+					}
+				}
+			})
+			.state('items.degenerator.2', {
+				url: '/2',
+				views: {
+					'items': {
+						templateUrl: 'html/degenerator/degenerator-sub-1.html'
+					},
+					'title@': {
+						template: '装备篇&nbsp;-&nbsp;<small>异界装备</small>'
+					}
+				}
+			})
+			.state('items.degenerator.3', {
+				url: '/3',
+				views: {
+					'items': {
+						templateUrl: 'html/degenerator/degenerator-sub-2.html'
+					},
+					'title@': {
+						template: '装备篇&nbsp;-&nbsp;<small>异界装备</small>'
+					}
+				}
+			})
+
+	$urlRouterProvider.otherwise('/');
+}]);
+document.body.onselectstart = function () {
+	return false;
+};
+
+document.oncut = function () {
+	return false;
+};
+document.oncopy = function () {
+	return false;
+};
+document.body.oncopy = function () {
+	return false;
+};
+
+app.run(function ($rootScope) {
+	$rootScope.$on('$viewContentLoading',
+			function (event, viewConfig) {
+				$rootScope.preloader = true;
+			});
+
+	$rootScope.$on('$viewContentLoaded',
+			function (event) {
+				$rootScope.preloader = false;
+			});
+
+});
+
+app.controller('main', ['$scope', function ($scope) {
+
+}]);
+(()=>{
+	document.body.onselectstart = function () {
+		return false;
+	};
+	document.oncut = function () {
+		return false;
+	};
+	document.oncopy = function () {
+		return false;
+	};
+	document.body.oncopy = function () {
+		return false;
+	};
+	document.oncut = function () {
+		return false;
+	};
+	document.body.oncut = function () {
+		return false;
+	};
+})();
+(function () {
+	try {
+		var $_console$$ = console;
+		Object.defineProperty(window, "console", {
+			get: function () {
+				if ($_console$$._commandLineAPI)
+					throw "抱歉, 为了用户安全, 本网站已禁用console脚本功能";
+				return $_console$$
+			},
+			set: function ($val$$) {
+				$_console$$ = $val$$
+			}
+		})
+	} catch ($ignore$$) {
+	}
+})();
+console.log("The year's at the spring");
+console.log("And day's at the morn");
+console.log("Morning's at seven");
